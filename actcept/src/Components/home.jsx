6d5b7ff1923bc458e8aba59428218f2f607fc3f2
hamzasaleem-1 <hamza.saleem@accenture.com>
2022-02-23 15:56:43 +0000

    Show events in rich card based format

--- actcept/src/Components/home.jsx
+++ actcept/src/Components/home.jsx
@@ -1,12 +1,21 @@
-import Event from "./event"
+import ShowEvents from "./event"
+import { useEffect, useState } from "react";
+import { fetchEvents } from "../api/api";
 
 const Home = () => {
+    const [events, setEvents] = useState([]);
+    useEffect(() => {
+        fetchEvents().then(setEvents);
+    }, []);
+
+
     return (<>
-    <div class="bgImage">
-        <h1> Welcome to ActCept </h1>
-        <p> See through the crowd and find your perfect event</p>
-    </div>
-        </>)
+        <div class="bgImage">
+            <h1> Welcome to ActCept </h1>
+            <p> See through the crowd and find your perfect event</p>
+            <ShowEvents events={events}/>
+        </div>
+    </>)
 
 }
 
