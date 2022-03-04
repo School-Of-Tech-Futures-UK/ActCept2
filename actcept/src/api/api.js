79cd334dc10e61f7ce9db1a00be957f65d734a32
YingyingEChen <yingying.e.chen@accenture.com>
2022-03-04 12:21:31 +0000

    change api access to domain url

--- actcept/src/api/api.js
+++ actcept/src/api/api.js
@@ -1,6 +1,6 @@
 export const fetchEvents = async () => {
   //  const results = await fetch("https://zuttio2meg.execute-api.eu-west-2.amazonaws.com/sprint1/events");
-  const results = await fetch('https://jt618wc5ik.execute-api.eu-west-2.amazonaws.com/prod/api/events')
+  const results = await fetch('https://events.sotf2022-01.com/api/events')
   const data = await results.json();
   const filteredData = await data.filter(x => x.status === 'confirmed')
   return filteredData;
@@ -8,7 +8,7 @@ export const fetchEvents = async () => {
 
 export const fetchEventData = async (id) => {
   // const url = `2/events`
-  const results = await fetch('https://jt618wc5ik.execute-api.eu-west-2.amazonaws.com/prod/api/events')
+  const results = await fetch('https://events.sotf2022-01.com/api/events')
   // console.log(results)
   const data = await results.json();
   // console.log(`My data ${data}`)
@@ -20,7 +20,7 @@ export const fetchEventData = async (id) => {
 export const getRegistrationInfo = async(id) => {
   console.log(`Reg ID ${id}`)
   const newID = Number(id)
-  const url = `https://79elonzas5.execute-api.eu-west-2.amazonaws.com/prod/api/get-registration/${newID}`
+  const url = `https://bookings.sotf2022-01.com/api/get-registration/${newID}`
   const results = await fetch(url)
   const data = await results.json();
   console.log(data)
@@ -28,7 +28,7 @@ export const getRegistrationInfo = async(id) => {
 };
 
 export const postRegistrationInfo = async (registrationInfo) => {
-  const response = await fetch('https://79elonzas5.execute-api.eu-west-2.amazonaws.com/prod/api/send-registration', {
+  const response = await fetch('https://bookings.sotf2022-01.com/api/send-registration', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -40,7 +40,7 @@ export const postRegistrationInfo = async (registrationInfo) => {
 }
 
 export const postReview = async (review) => {
-  const  response = await fetch('https://79elonzas5.execute-api.eu-west-2.amazonaws.com/prod/api/send-review', {
+  const  response = await fetch('https://bookings.sotf2022-01.com/api/send-review', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -52,7 +52,7 @@ export const postReview = async (review) => {
 }
 
 export const getAllReviews = async () => {
-  const url = `https://79elonzas5.execute-api.eu-west-2.amazonaws.com/prod/api/getallreviews`
+  const url = `https://bookings.sotf2022-01.com/api/getallreviews`
   const results = await fetch(url)
   const data = await results.json();
   console.log('getAllReviews has been called')
@@ -62,7 +62,7 @@ export const getAllReviews = async () => {
 
 export const fetchRegistrations = async (id) => {
   const newID = Number(id)
-  const results = await fetch(`https://79elonzas5.execute-api.eu-west-2.amazonaws.com/prod/api/get-registration/${newID}`)
+  const results = await fetch(`https://bookings.sotf2022-01.com/api/get-registration/${newID}`)
   const data = await results.json();
   return data;
 };
