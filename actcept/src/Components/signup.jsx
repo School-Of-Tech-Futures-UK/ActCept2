71fd3e854acc3fd795a7a3b3246f12db4ec4bdc2
hamzasaleem-1 <hamza.saleem@accenture.com>
2022-03-10 10:09:34 +0000

    Icon Test

--- actcept/src/Components/signup.jsx
+++ actcept/src/Components/signup.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { postRegistrationInfo, fetchRegistrations, fetchEventData} from "../api/api";
 import '../App.css';
-// import { BsChevronLeft } from "react-icons/bs";
+import { BsChevronLeft } from "react-icons/bs";
 
 function UserDetails() {
 	const { id } = useParams();
@@ -51,7 +51,7 @@ function UserDetails() {
 
 	return (<>
 		<div class="backgroundProperties">
-			<Link type="button" to={`/event-page/${id}`} class="returnButton">Return to event</Link>
+			<Link type="button" to={`/event-page/${id}`} class="returnButton"><BsChevronLeft/>Return to event</Link>
 			<div id="eventTicketFlex">
 				<div id="eventTicketInput">
 				<h3>Your ticket to <strong> {eventData[0].event_name} </strong> </h3>
